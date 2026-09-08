8db4ac4a30c557d6d47eb24f8a594b4d6bcbf393
Henning Otte <HenningOtte91@gmail.com>
2026-09-08 16:02:29 +0200

    Add profile menu with logout functionality

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,29 +1,82 @@
+import { useState, useContext } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
 
 function Navbar() {
+  const authContext = useContext(AuthContext);
+  const toggleProfilMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const handleLogout = () => {
+    localStorage.setItem("token", "");
+    authContext?.setLoggedIn(false);
+    authContext?.setUser(() => {
+      return {
+        firstname: "",
+        lastname: "",
+        email: "",
+      };
+    });
+    toggleProfilMenu();
+  };
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="navbar">
-      <img
-        className="logo"
-        src="./src/assets/icons/currio-logo.svg"
-        alt="currio-logo"
-      />
-      <nav className="navbar-actions">
-        <Link className="navbar-link" to={"/"}>
-          Home
-        </Link>
-        <Link className="navbar-link" to={"/favorites"}>
-          Favorites
-        </Link>
-        <Link to={"/profil"}>
-          <img
-            className="navbar-avatar"
-            src="./src/assets/icons/profile-pic.svg"
-            alt="profil-pic"
-          />
-        </Link>
+    <div className="navbar-container">
+      <nav className="navbar">
+        <img
+          className="logo"
+          src="./src/assets/icons/currio-logo.svg"
+          alt="currio-logo"
+        />
+        <div className="navbar-actions">
+          <Link
+            onClick={() => {
+              if (isMenuOpen) toggleProfilMenu();
+            }}
+            className="navbar-link"
+            to={"/"}
+          >
+            Home
+          </Link>
+          <Link
+            onClick={() => {
+              if (isMenuOpen) toggleProfilMenu();
+            }}
+            className="navbar-link"
+            to={"/favorites"}
+          >
+            Favorites
+          </Link>
+          <button
+            onClick={() => {
+              toggleProfilMenu();
+            }}
+          ></button>
+        </div>
       </nav>
+      <div className={isMenuOpen ? "profileMenu" : "dNone"}>
+        <Link
+          onClick={() => {
+            toggleProfilMenu();
+          }}
+          className="profileMenu-link"
+          to={"/profil"}
+        >
+          Profile
+        </Link>
+        <button
+          onClick={() => {
+            toggleProfilMenu();
+            handleLogout();
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
